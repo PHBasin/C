fe2ecda1353216965157c1d26e1dcc263f3dca73
Pierre-Henri Basin <pierre-henri.basin@orange.fr>
2020-07-16 18:18:27 +0400

    update

---
+++ OpenClassroom/malloc.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  int* memoireAllouee = NULL;
+  
+  memoireAllouee = malloc(sizeof(int));
+  if (memoireAllouee == NULL)
+  {
+    exit(0);
+  }
+  
+  return 0;
+}
+
